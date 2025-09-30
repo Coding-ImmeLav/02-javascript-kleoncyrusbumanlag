a7a3dc0bb46d52da2d00308e334ca6bffc5c843f
kleoncyrusbumanlag <kleon.bumanlag@edu.immaginazioneelavoro.it>
2025-09-30 09:27:17 +0200

    sheeeesh

---
+++ js.js
@@ -0,0 +1,17 @@
+console.log("Ciao")
+
+function nero() {
+    document.getElementById("quadrato").style.backgroundColor = "black";
+}
+
+function giallo() {
+    document.getElementById("quadrato").style.backgroundColor = "yellow";
+}
+
+function rosa() {
+    document.getElementById("quadrato").style.backgroundColor = "pink";
+}
+
+function blu() {
+    document.getElementById("quadrato").style.backgroundColor = "blue";
+}
